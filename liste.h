9445e9a2a810c476e9bd9bdc0aa2765a7d1a6c47
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-03-02 23:24:15 +0100

    Create liste.h

---
+++ liste.h
@@ -0,0 +1,14 @@
+#ifndef _LISTE_H_
+#define _LISTE_H_
+
+/* Definition d'une liste chainée */
+
+typedef struct _liste_chainee{
+    int data;
+    struct _liste_chainee * next;
+}_liste_chainee;
+
+/*------------------------------------------------*/
+
+
+#endif
