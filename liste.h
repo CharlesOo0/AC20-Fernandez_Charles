76f5283abc7b5f0fc67eed1c6ca76b787f0a9508
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-03-04 21:42:58 +0100

    Update liste.h

--- liste.h
+++ liste.h
@@ -11,10 +11,21 @@ typedef struct _liste_chainee { // Ok pour le nom défini avant le contenu de la
 
 // On doit exporter les fonctions avec leur déclaration (la signature) dans le .h
 // Ces fonctions seront ensuite utilisables depuis le main avec la directive #include "liste.h"
+
+/* LISTE CHAINEES SIMPLE */
+
 void erase_list(liste_chainee_t* head);
 void display_list(liste_chainee_t* head);
 liste_chainee_t *add_head_list(liste_chainee_t* head, int data);
 liste_chainee_t *add_tail_list(liste_chainee_t* head, int data);
+int list_length(liste_chainee_t* head);
+liste_chainee_t *add_target_list(liste_chainee_t* head, int data, int pos);
+liste_chainee_t *delete_head_list(liste_chainee_t* head);
+liste_chainee_t *delete_tail_list(liste_chainee_t* head);
+liste_chainee_t *delete_target_list(liste_chainee_t* head, int pos);
+
+/* LISTE DOUBLEMENT CHAINEES */
+
 
 // Une liste chaînée est construite élément par élément, elle ne peut donc pas être créée à l'avance.
 // On retire donc cette fonction des listes
