4c7b2955eba7f237ae19e674e188729e3a5df526
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-04-10 01:58:20 +0200

    Update arbre.h

--- arbre.h
+++ arbre.h
@@ -1,6 +1,23 @@
 #ifndef _ARBRE_H_
 #define _ARBRE_H_
 
+ /* Définition d'un arbre */
 
+ typedef struct _arbre{
+    struct _arbre* Agauche;
+    struct _arbre* Adroite;
+    int data;
+ }arbre_t;
 
+ /* Prototypes arbre */
+
+ void erase_tree(arbre_t *tree);
+ arbre_t *create_tree(int data);
+ void display_tree_preorder(arbre_t *root);
+ void display_tree_inorder(arbre_t *root);
+ void display_tree_postorder(arbre_t *root);
+ arbre_t *add_tree(arbre_t *tree, int data, int leftOrRight);
+ arbre_t *tree_maker(int nodes[], size_t lengthNodes);
+
+ 
 #endif
