dbcefdff16e56f5c2cbb9af19434ef83520c87cd
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-04-11 18:19:23 +0200

    Update arbre_binaire.h

--- arbre_binaire.h
+++ arbre_binaire.h
@@ -1,6 +1,10 @@
 #ifndef _ARBRE_H_
 #define _ARBRE_H_
 
+ /* Définition d'un booléen */
+
+ typedef enum { false, true } bool;
+
  /* Définition d'un arbre */
 
  typedef struct _arbre{
@@ -16,8 +20,10 @@
  void display_tree_preorder(arbre_t *root);
  void display_tree_inorder(arbre_t *root);
  void display_tree_postorder(arbre_t *root);
- arbre_t *add_tree(arbre_t *tree, int data, int leftOrRight);
+ void add_tree(arbre_t *root, int target, int data, int leftOrRight);
  arbre_t *tree_maker(int nodes[], size_t lengthNodes);
-
+ int node_counting(arbre_t *root);
+ int get_parent(arbre_t *root, int target, int parent);
+ bool is_node(arbre_t *root, int target);
  
 #endif
