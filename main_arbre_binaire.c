ce292801652403e3c73c83c4f9a32f34ae93607f
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-04-11 18:19:49 +0200

    Update main_arbre_binaire.c

--- main_arbre_binaire.c
+++ main_arbre_binaire.c
@@ -10,27 +10,43 @@ int main() {
 
     // Crée un arbre manuellement
     un_arbre = create_tree(1);
-
-    un_arbre->Agauche = create_tree(2);
-        un_arbre->Agauche->Agauche = create_tree(3);
-        un_arbre->Agauche->Adroite = create_tree(3);
-
-    un_arbre->Adroite = create_tree(2);
-        un_arbre->Adroite->Agauche = create_tree(3);
-        un_arbre->Adroite->Adroite = create_tree(3);
+        add_tree(un_arbre,1,2,0);
+            add_tree(un_arbre,2,4,0);
+            add_tree(un_arbre,2,5,1);
+        add_tree(un_arbre,1,3,1);
+            add_tree(un_arbre,3,6,0);
+            add_tree(un_arbre,3,7,1);
     
     display_tree_postorder(un_arbre);
     printf("\n");
 
-    erase_tree(un_arbre);
+    printf("Le parent de 2 est %d\n",get_parent(un_arbre,6,0));
+
+    if (is_node(un_arbre,15)) {
+        printf("Noeud présent\n");
+    }else {
+        printf("Noeud absent\n");
+    }
+
+    //erase_tree(un_arbre);
     un_arbre = NULL;
     
 
-    int nodes[7] = {1,2,2,3,3,3,3};
+    int nodes[7] = {1,5,7,2,8,4,1};
+    // Représentation d'un arbre par étage en quelque sorte
+    // On écrit l'étage 1 de gauche à droite
+    // Puis l'étage 2 toujours de gauche à droite etc
+    //               1
+    //              / \ 
+    //             5   7
+    //            / \ / \ 
+    //           2  8 4  1
 
     un_arbre = tree_maker(nodes, sizeof(nodes)/sizeof(int));
 
     display_tree_postorder(un_arbre);
+
+    printf("number of nodes : %d",node_counting(un_arbre));
 
     return EXIT_SUCCESS;
 }
