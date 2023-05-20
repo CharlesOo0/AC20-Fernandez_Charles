62db9dc07dc412965b0f0bb603a5b38d154267e2
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-05-20 20:48:07 +0200

    Update graphe.h

--- graphe.h
+++ graphe.h
@@ -1,13 +1,6 @@
 #ifndef _GRAPHE_H_
 #define _GRAPHE_H_
 
-    /* Définition d'un booléen */
-
-typedef enum{
-    false,
-    true
-}Bool;
-
     /* Définition d'un graphe */
 
 /* La liste des noeuds liées */
@@ -28,21 +21,33 @@ typedef struct _adjencyList {
 /* Le graphe */
 
 typedef struct _graph {
-    Bool is_oriented;
-    int nb_vertices;
+    bool is_oriented;
+    int memory_allocated;
+    int tab_memory;
     adjencyList_t *tab;
 }graph_t;
 
+/* Djikstra Node */
+
+typedef struct _node_djikstra {
+    int data;
+    bool seen;
+    int *path;
+    int weight;
+}node_d_t;
+
 /* Prototypes */
 
-graph_t *create_graph(int vertices, Bool is_oriented);
+graph_t *create_graph(int vertices, bool is_oriented);
 void erase_graph(graph_t* g);
-Bool is_empty(graph_t *g);
+bool is_empty(graph_t *g);
 int contain(graph_t *g, int target);
 graph_t *add_vertex(graph_t *g, int vertex);
 graph_t *remove_vertex(graph_t *g, int target);
 graph_t *add_edge(graph_t* g, int src, int dst, int weight);
 graph_t *remove_edge(graph_t* g, int src, int dst);
 void display_graphe(graph_t *g);
+adjencyList_t *increase_memory(graph_t* g);
+adjencyList_t *reduce_memory(graph_t* g);
 
 #endif
