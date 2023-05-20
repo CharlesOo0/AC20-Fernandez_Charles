df116f419ffef4c05c8b2ebe906a3362ca9f08ca
CharlesOo0 <104007616+CharlesOo0@users.noreply.github.com>
2023-05-20 20:47:16 +0200

    Update main_graphe.c

--- main_graphe.c
+++ main_graphe.c
@@ -4,6 +4,7 @@
 
 int main() {
 
+    /*
     graph_t *g = create_graph(5,true);
 
     display_graphe(g);
@@ -14,21 +15,46 @@ int main() {
 
     g = create_graph(3,true);
 
-    g = add_vertex(g, 47801);
+    for (int i = 4; i < 22; ++i) {
+        g = add_vertex(g, i);
+    }
+    printf("\n%d %d",g->memory_allocated,g->tab_memory);  
+    printf("\n %d ",g->tab[0].data);
 
-    g = remove_vertex(g,2);
+    for (int j = 0; j < 23; ++j) {
+        g = remove_vertex(g, j);
+    }
 
-    display_graphe(g);
-
-    //printf("\n dqsdqs %d",g->tab[3].data); // Problème à résoudre le réalloc n'enlève pas le noeud du tableau
-
-    g = add_edge(g,1,3,15);
+    g = add_edge(g,2,1,15);
     g = add_edge(g,3,1,15);
-    g = add_edge(g,1,47801,45);
+    g = add_edge(g,4,1,15);
 
-    g = remove_edge(g,3,1);
+    g = remove_vertex(g,1);
 
     display_graphe(g);
+
+    g = add_edge(g,4,5,15);
+    g = add_edge(g,4,6,15);
+    g = add_edge(g,5,6,15);
+    g = add_edge(g,5,14,15);
+    g = add_edge(g,5,13,15);
+
+    g = remove_vertex(g,14);
+
+    display_graphe(g);
+
+    //printf("\ndzqhdzhq %d",5/10);*/
+
+    graph_t *g = create_graph(5,true);
+    g = add_edge(g,1,3,20);
+    g = add_edge(g,1,5,3);
+    g = add_edge(g,3,2,4);
+    g = add_edge(g,4,2,1);
+    g = add_edge(g,5,2,10);
+    g = add_edge(g,5,4,2);
+
+    display_graphe(g);
+
 
     return EXIT_SUCCESS;
 }
